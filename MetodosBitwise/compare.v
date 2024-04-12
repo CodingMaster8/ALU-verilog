d454f95f627d00f0120a7d3e690485495b85a760
Pablo Vargas <A00344869@tec.mx>
2024-04-11 21:31:20 -0500

    Create compare.v

---
+++ MetodosBitwise/compare.v
@@ -0,0 +1,30 @@
+module compare(
+	
+	reg[31:0] i_bit1,
+	reg [31:0] i_bit2,
+	
+	wire [31:0] w_VECTOR
+	
+	o_bit;
+
+)
+
+
+	input i_bit1;
+	input i_bit2;
+	output o_bit;
+	
+	assign w_VECTOR = i_bit1 & i_bit2;
+	
+	initial begin
+		if (w_VECTOR == 32b'11111111111111111111111111111111) begin
+			o_bit = 1b'1;
+		
+		end
+		
+		else
+			o_bit = 1b'0;
+	end
+endmodule
+		
+
