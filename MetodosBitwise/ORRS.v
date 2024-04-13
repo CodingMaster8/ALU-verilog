cb7fa98f915cddd3c243104a4b9a42d75b25cf02
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-13 11:59:52 -0600

    Update ORRS.v

--- MetodosBitwise/ORRS.v
+++ MetodosBitwise/ORRS.v
@@ -1,11 +1,9 @@
 module ORRS(
-	input [31:0] i_bit1,
-	input [31:0] i_bit2,
+	input [31:0] a,
+	input [31:0] b,
+	output [31:0] o
+);
+
+assign o = a | b;
 	
-	output [31:0] o_bit
-	
-)
-	
-	assign o_bit = i_bit1 | i_bit2;
-	
-endmodule
+endmodule 
