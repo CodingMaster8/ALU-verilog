699f5f20a2140b7a2b9a25cbd73a0ac4b3e02d64
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-13 12:12:55 -0600

    Create SBCS.v

---
+++ MetodosBitwise/SBCS.v
@@ -0,0 +1,27 @@
+module SBCS(
+	input [31:0] a, b,
+	output [31:0] s,
+	output c_out
+);
+
+wire [31:0] w_s;
+
+ADD_ADCS_SUBS aas0(
+.a(a),
+.b(b),
+.c_in(1),
+.op(1),
+.s(w_s),
+.c_out()
+);
+
+ADD_ADCS_SUBS aas1(
+.a(w_s),
+.b(1),
+.c_in(1),
+.op(1),
+.s(s),
+.c_out(c_out)
+);
+
+endmodule 
