229c752303ffbea341c529438e9fd0c1164d3e13
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 17:22:45 -0600

    Update SBCS_tb.v

--- MetodosBitwise/SBCS_tb.v
+++ MetodosBitwise/SBCS_tb.v
@@ -3,14 +3,21 @@ module SBCS_tb();
 reg [31:0] a, b;
 wire [31:0] s;
 wire c_out;
+wire n, z, c, v;
 
+// Instancia de la cama de pruebas
 SBCS s0(
 .a(a),
 .b(b),
 .s(s),
-.c_out(c_out)
+.c_out(c_out),
+.n(n),
+.z(z),
+.c(c),
+.v(v)
 );
 
+// Estimulo
 initial
 begin
 	a = 13409;
