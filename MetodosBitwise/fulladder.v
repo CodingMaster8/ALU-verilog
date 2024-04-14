c9144c58d83a9071248b2e3ab34f24e1a99eea53
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 17:24:54 -0600

    Update fulladder.v

--- MetodosBitwise/fulladder.v
+++ MetodosBitwise/fulladder.v
@@ -1,12 +1,13 @@
 module fulladder(
-	input 	a,
-				b,
-				c_in,
-	output	s,
-				c_out
+	input 	a,			// Operando 1
+		b,			// Operando 2
+		c_in,			// Acarreo de entrada
+	output	s,			// Resultado
+		c_out			// Acarreo de salida
 );
 
-assign s = a ^ b ^ c_in;
-assign c_out = (a & b) + ((a ^ b) & c_in);
+
+assign s = a ^ b ^ c_in;							// Calculo de la suma de 2 bits
+assign c_out = (a & b) | ((a ^ b) & c_in);	// Calculo acarreo de 2 bits
 
 endmodule 
