51146b65116b6ea03175403d0bcc44852fd6da5e
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 10:54:01 -0600

    Update CMP.v

--- MetodosBitwise/CMP.v
+++ MetodosBitwise/CMP.v
@@ -1,10 +1,11 @@
 module CMP(
-	input [31:0] a,b,
-	output n,
-	output z,
-	output c,
-	output v
+	input [31:0] a, b,
+	output wire n,
+	output wire z,
+	output wire c,
+	output wire v
 );
+	
 
 wire [31:0] s;
 wire c_out;
@@ -23,5 +24,6 @@ assign c = c_out;
 assign n = s[31];
 assign z = ~(s[0] | s[1] | s[2] | s[3] | s[4] | s[5] | s[6] | s[7] | s[8] | s[9] | s[10] | s[11] | s[12] | s[13] | s[14] | s[15] | s[16] | s[17] | s[18] | s[19] | s[20] | s[21] | s[22] | s[23] | s[24] | s[25] | s[26] | s[27] | s[28] | s[29] | s[30] | s[31]);
 assign v = (a[31] & ~b[31] & ~s[31]) | (~a[31] & b[31] & s[31]);
+	
 	
 endmodule 
