06aacd5ef97ac605af48364c6988fd78d74ab3c8
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 09:24:49 -0600

    Update and rename ADD_ADCS_SUBS_tb.v to ADD_ADCS_SUBS_RSBS_tb.v

--- MetodosBitwise/ADD_ADCS_SUBS_RSBS_tb.v
+++ MetodosBitwise/ADD_ADCS_SUBS_RSBS_tb.v
@@ -1,16 +1,18 @@
-module ADD_ADCS_SUBS_tb();
+module ADD_ADCS_SUBS_RSBS_tb();
 
 reg	[31:0] a, b;
 reg	c_in;
 reg	op;
+reg	rev;
 wire	[31:0] s;
 wire	c_out;
 
-ADD_ADCS_SUBS DUT(
+ADD_ADCS_SUBS_RSBS DUT(
 .a(a),
 .b(b),
 .c_in(c_in),
 .op(op),
+.rev(rev),
 .s(s),
 .c_out(c_out)
 );
@@ -19,27 +21,37 @@ initial
 begin
 	a = 82347156;
 	b = 9483;
+	op = 0;
 	c_in = 0;
-	op = 0;
+	rev = 0;
 	#10
-	c_in = 1;
 	op = 0;
+	c_in = 0;
+	rev = 1;
+	#10
+	op = 0;
+	c_in = 1;
+	rev = 0;
+	#10
+	op = 0;
+	c_in = 1;
+	rev = 1;
 	#10
 	op = 1;
-	#10
 	c_in = 0;
-	#10
-	a = 9483;
-	b = 82347156;
-	c_in = 0;
-	op = 0;
-	#10
-	c_in = 1;
-	op = 0;
+	rev = 0;
 	#10
 	op = 1;
-	#10
 	c_in = 0;
+	rev = 1;
+	#10
+	op = 1;
+	c_in = 1;
+	rev = 0;
+	#10
+	op = 1;
+	c_in = 1;
+	rev = 1;
 	#10
 	$stop;
 end
