f4aaabcc166da7c5d513ae6587b2d84e0d27b15b
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 11:16:51 -0600

    Update ADD_ADCS_SUBS_RSBS.v

--- MetodosBitwise/ADD_ADCS_SUBS_RSBS.v
+++ MetodosBitwise/ADD_ADCS_SUBS_RSBS.v
@@ -3,8 +3,9 @@ module ADD_ADCS_SUBS_RSBS(
 	input	c_in,
 	input op,
 	input rev,
-	output [31:0] s, 
-	output c_out
+	output [31:0] s,
+	output c_out,
+	output n, z, c ,v
 );
 
 wire [30:0] w_c_out;
@@ -27,5 +28,10 @@ generate
 endgenerate
 
 fulladder fa1(.a(w_a[31]),.b(w_b[31]),.c_in(w_c_out[30]),.s(s[31]),.c_out(c_out));
+
+assign n = s[31];
+assign z = ~(s[0] | s[1] | s[2] | s[3] | s[4] | s[5] | s[6] | s[7] | s[8] | s[9] | s[10] | s[11] | s[12] | s[13] | s[14] | s[15] | s[16] | s[17] | s[18] | s[19] | s[20] | s[21] | s[22] | s[23] | s[24] | s[25] | s[26] | s[27] | s[28] | s[29] | s[30] | s[31]);
+assign c = c_out;
+assign v = op ? (a[31] & ~b[31] & ~s[31]) | (~a[31] & b[31] & s[31]) : (~a[31] & ~b[31] & s[31]) | (a[31] & b[31] & ~s[31]);
 
 endmodule 
