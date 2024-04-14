aac5d5d76257d2c6c1baad232e60f5ebff5527ee
Noé Benjamín Rosales Medina <145573317+NoeRosales22@users.noreply.github.com>
2024-04-14 16:48:16 -0600

    Update ADD_ADCS_SUBS_RSBS_tb.v

--- MetodosBitwise/ADD_ADCS_SUBS_RSBS_tb.v
+++ MetodosBitwise/ADD_ADCS_SUBS_RSBS_tb.v
@@ -6,7 +6,9 @@ reg	op;
 reg	rev;
 wire	[31:0] s;
 wire	c_out;
+wire n, z, c, v;
 
+// Instancia para cama de pruebas
 ADD_ADCS_SUBS_RSBS DUT(
 .a(a),
 .b(b),
@@ -14,13 +16,18 @@ ADD_ADCS_SUBS_RSBS DUT(
 .op(op),
 .rev(rev),
 .s(s),
-.c_out(c_out)
+.c_out(c_out),
+.n(n),
+.z(z),
+.c(c),
+.v(v)
 );
 
+// Estimulos para ocho casos de prueba
 initial
 begin
-	a = 82347156;
-	b = 9483;
+	a = 32'h8_0_0_0_0_0_0_0;
+	b = 32'h7_F_F_F_F_F_F_F;
 	op = 0;
 	c_in = 0;
 	rev = 0;
